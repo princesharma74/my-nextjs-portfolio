1a069d4b760ce9ffa9d6954f0b2a7b56524cbfa5
Prince Sharma <princesharma2899@gmail.com>
2024-06-25 22:36:51 +0530

    Checkpoint Jun 25

--- lib/pb.ts
+++ lib/pb.ts
@@ -1,3 +1,20 @@
 import PocketBase from 'pocketbase';
-const pb = new PocketBase(process.env.POCKETBASE_URL);
-export default pb;
+declare global {
+    var pb: PocketBase | undefined;
+  }
+  
+  const pb = global.pb || new PocketBase(process.env.POCKETBASE_URL);
+  const token = process.env.TOKEN;
+  if(token){
+    pb.authStore.save(token, {verified : false})
+  }
+  pb.beforeSend = function (url, options) {
+    options.timeout = 10000; // Increase timeout to 10 seconds
+    return { url, options };
+  };
+  
+  if (process.env.NODE_ENV !== 'production') {
+    global.pb = pb;
+  }
+
+  export default pb;
