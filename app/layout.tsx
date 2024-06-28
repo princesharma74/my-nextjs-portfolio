31c4a5fe785f3e03c87d8ddba33a2ed670fd91e0
Prince Sharma <princesharma2899@gmail.com>
2024-06-28 17:17:17 +0530

    Added responsibility and achievements separately

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           <ModalProvider/>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
